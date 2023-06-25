b7e4b79a9f3e120303355d0b1dfd39e78c02f3ce
netol <figlovskayad@gmail.com>
2023-06-25 18:05:00 +0300

    platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -24,4 +24,25 @@ public class CashbackHackServiceTest {
 
 
     }
+    @org.junit.jupiter.api.Test
+    public void testRemain5() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainLessThenMinAmount5() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        assertEquals(actual, expected);
+
+
+    }
 }
