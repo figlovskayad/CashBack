10c9321a7d22873f575e53faa6398cc09b7ee728
netol <figlovskayad@gmail.com>
2023-06-25 17:49:19 +0300

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology.service;
+import static org.testng.Assert.assertEquals;
+public class CashbackHackServiceTest {
+    @org.testng.annotations.Test
+    public void testRemain(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+
+
+    }
+    @org.testng.annotations.Test
+    public void testRemainLessThenMinAmount(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        assertEquals(actual, expected);
+
+
+    }
+}
